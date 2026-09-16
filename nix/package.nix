b7859de60514ab9129902c84f1a2308ac11fe9e3
Lunarch <phantanlocct@outlook.com>
2026-09-16 11:57:21 +0700

    fix(packaging): ship license in Nix output

--- nix/package.nix
+++ nix/package.nix
@@ -257,6 +257,7 @@ EOF
     for doc in docs/*.md; do
       install -Dm644 "$doc" "$docs/${doc##*/}"
     done
+    install -Dm644 LICENSE "$out/share/licenses/inir/LICENSE"
 
     runHook postInstall
   '';
